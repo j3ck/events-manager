2b9cc8fce7a02036dce13dba3afff1577efd9685
j3ck <fox_raiodin@mail.ru>
2014-05-18 15:54:24 +0300

    added time and date in event controller and event views

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -71,6 +71,6 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params.require(:event).permit(:title, :status, :description, :user_id, :time, :date)
+      params.require(:event).permit(:title, :status, :description, :user_id, :date, :time)
     end
 end
