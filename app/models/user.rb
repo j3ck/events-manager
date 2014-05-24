3c8dd29c99fae7aeba0c3a9bd561b0c76cd4319d
j3ck <fox_raiodin@mail.ru>
2014-05-24 14:12:57 +0300

    change user with participant through assotiation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
 
 	has_many :events
 	has_many :participations
+	has_many :participant_events, through: :participations, source: "event"
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
