14292558d9c2f597224ec30ec308f1d101b246dd
j3ck <fox_raiodin@mail.ru>
2014-06-15 21:03:01 +0300

    refactor comments/delete to JSON format

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -2,6 +2,11 @@ $(document).ready(function() {
 	PrivatePub.subscribe("/comments/new", function(data, channel) {
 	  appendComment(data.create_status, data.form_html, data.comment_html)
 	})
+    PrivatePub.subscribe("/comments", function(data, channel) {
+      $('li[data-id=data.comment.id]').fadeOut('slow', function() {
+        $(this).remove();
+      })
+    })
 })
 
 function appendComment(status, formHTML, commentHTML, book) {
