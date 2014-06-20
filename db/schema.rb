7437937d167cfb047f1e755e40288e5fae3d4fcd
j3ck <fox_raiodin@mail.ru>
2014-06-20 17:17:04 +0300

    remove last_name, first_name from the users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140614173716) do
+ActiveRecord::Schema.define(version: 20140620141257) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,8 +95,6 @@ ActiveRecord::Schema.define(version: 20140614173716) do
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
-    t.string   "first_name"
-    t.string   "last_name"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
