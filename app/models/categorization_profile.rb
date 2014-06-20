f5e7980ea446c33d858da20a089ac6b14eec9a1d
j3ck <fox_raiodin@mail.ru>
2014-06-20 23:55:02 +0300

    added association for profiles-categories

--- app/models/categorization_profile.rb
+++ app/models/categorization_profile.rb
@@ -1,2 +1,4 @@
 class CategorizationProfile < ActiveRecord::Base
+	belongs_to :category
+	belongs_to :profile
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,6 @@
 class Profile < ActiveRecord::Base
 	belongs_to :user
+
+	has_many :categorization_profiles
+	has_many :categories, through: :categorization_profiles
 end
