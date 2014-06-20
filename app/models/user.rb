9d23743064d34552011cf1d1d64397926abf2cfc
j3ck <fox_raiodin@mail.ru>
2014-06-20 20:56:18 +0300

    added after_create callback for create user-profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,12 @@ class User < ActiveRecord::Base
 	has_many :participant_events, through: :participations, source: "event"
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+
+  after_create :add_profile
+
+  private
+  	def add_profile
+  		self.build_profile.save
+  	end
 end
