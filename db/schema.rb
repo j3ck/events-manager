0d72c107aaaace67f85316306cf8b07638883b78
j3ck <fox_raiodin@mail.ru>
2014-06-27 00:12:38 +0300

    position column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140625100529) do
+ActiveRecord::Schema.define(version: 20140626195001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -94,6 +94,7 @@ ActiveRecord::Schema.define(version: 20140625100529) do
     t.datetime "photo_updated_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "position"
   end
 
   create_table "profiles", force: true do |t|
