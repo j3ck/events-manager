fd59bc9c44263b9950f01e94b6169167cbbf9cba
j3ck <fox_raiodin@mail.ru>
2014-07-13 18:34:27 +0300

    added email confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ActiveRecord::Base
 	has_many :participations
 	has_many :participant_events, through: :participations, source: "event"
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   after_create :add_profile
 
