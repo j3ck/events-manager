3575e4566329554106365470bf554da84ea595c5
j3ck <fox_raiodin@mail.ru>
2014-07-16 15:24:48 +0300

    added feed variables to the controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,2 +1,19 @@
 class StaticPagesController < ApplicationController
+
+	def home
+		@events = Event.order("time ASC").take(5)
+		@tags = Tag.order("created_at ASC")
+		@top_users = top_users
+	end
+
+	private
+
+	def top_users
+		users = Hash.new
+		User.all.each do |user|
+			users[user] = user.participant_events.where(status: false).count
+		end
+		users = Hash[users.sort { |a,b| b[1] <=> a[1] }]
+	end
+
 end
