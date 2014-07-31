0778aede1c945c837463069570c0eed3a9b36613
j3ck <fox_raiodin@mail.ru>
2014-07-31 12:30:13 +0300

    added missing avatar file

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,6 +4,6 @@ class Profile < ActiveRecord::Base
 	has_many :categorization_profiles
 	has_many :categories, through: :categorization_profiles
 
-	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/missing.gif"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
