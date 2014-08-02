b9769f0ddc56aac5e6653e091c7c03da2b7259d5
j3ck <fox_raiodin@mail.ru>
2014-08-02 11:51:29 +0300

    refactor search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,19 +1,19 @@
 class SearchController < ApplicationController
 
 	def index
-		if params[:section] == "all"
-			@events = Event.search params[:q], limit: 2
-			@profiles = Profile.search params[:q], limit: 2
-			@tags = Tag.search params[:q], limit: 2
-			@categories = Category.search params[:q], limit: 2
-		elsif params[:section] == "event"
-			@events = Event.search params[:q]
-		elsif params[:section] == "profile"
-			@profiles = Profile.search params[:q]
-		elsif params[:section] == "tag"
-			@tags = Tag.search params[:q]
-		elsif params[:section] == "category"
-			@categories = Category.search params[:q]
+		if params[:q].present?
+			if params[:section] == "all"
+				@results = { events: Event, profiles: Profile, tags: Tag, categories: Category }
+				@results.each { |k,v| @results[k] = v.search params[:q], limit: 2 }
+			elsif params[:section] == "event"
+				@events = Event.search params[:q]
+			elsif params[:section] == "profile"
+				@profiles = Profile.search params[:q]
+			elsif params[:section] == "tag"
+				@tags = Tag.search params[:q]
+			elsif params[:section] == "category"
+				@categories = Category.search params[:q]
+			end
 		end
 	end
 end
