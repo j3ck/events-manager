f0cf4d39549223876d588cfabc8885fba69cec7b
j3ck <fox_raiodin@mail.ru>
2014-08-06 23:27:34 +0300

    fix avatar large style changes, up avatar small style to 200x200

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,7 +4,7 @@ class Profile < ActiveRecord::Base
 	has_many :categorization_profiles
 	has_many :categories, through: :categorization_profiles
 
-	has_attached_file :avatar, :styles => { :small => "100x100#", :large => "500x500>" }, :default_url => "/missing.gif", :processors => [:cropper]
+	has_attached_file :avatar, :styles => { :small => {:geometry => "200x200#", :processors => [:cropper]}, :large => {:geometry => "500x500>"} }, :default_url => "/missing.gif"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
   attr_accessor :crop_x, :crop_y, :crop_w, :crop_h
