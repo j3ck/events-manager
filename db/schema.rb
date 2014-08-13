594c4df5130fa5206b12cb3e2955944e907670ed
j3ck <fox_raiodin@mail.ru>
2014-08-13 19:03:14 +0300

    added avatar fields to event

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140809154249) do
+ActiveRecord::Schema.define(version: 20140813151404) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,7 +75,11 @@ ActiveRecord::Schema.define(version: 20140809154249) do
     t.string   "place"
     t.float    "latitude"
     t.float    "longitude"
-    t.boolean  "status",      default: true
+    t.boolean  "status",              default: true
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "participations", force: true do |t|
